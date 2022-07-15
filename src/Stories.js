69978f3daaf22e5d2fac82f12fd8fff41b2a3e45
Yan Amaral <yan@pop-os.localdomain>
2022-07-15 12:25:16 -0300

    Componentização stories com map

--- src/Stories.js
+++ src/Stories.js
@@ -1,34 +1,43 @@
 function Story(props) {
-    return (
-      <div class="story">
-        <div class="imagem">
-          <img src={props.img} />
-        </div>
-        <div class="usuario">{props.user}</div>
+  return (
+    <div class="story">
+      <div class="imagem">
+        <img src={props.img} />
       </div>
-    );
-  }
-  
-  function Stories () {
-      return (
-          <div class="stories">
-              <Story img="assets/img/9gag.svg" user="9gag" />
-              <Story img="assets/img/meowed.svg" user="meowed" />
-              <Story img="assets/img/barked.svg" user="barked" />
-              <Story
-                  img="assets/img/nathanwpylestrangeplanet.svg"
-                  user="9nathanwpylestrangeplanet"
-              />
-              <Story img="assets/img/wawawicomics.svg" user="wawawicomics" />
-              <Story img="assets/img/respondeai.svg" user="respondeai" />
-              <Story img="assets/img/filomoderna.svg" user="filomoderna" />
-              <Story img="assets/img/memeriagourmet.svg" user="memeriagourmet" />
-  
-              <div class="setinha">
-                  <ion-icon name="chevron-forward-circle"></ion-icon>
-              </div>
-              </div>
-      );
-  }
+      <div class="usuario">{props.user}</div>
+    </div>
+  );
+}
 
-  export default Stories
+const storiesArray = [
+  {img:"assets/img/9gag.svg", user:"9gag"},
+  {img:"assets/img/meowed.svg", user:"meowed"},
+  {img:"assets/img/barked.svg", user:"barked"},
+  {img:"assets/img/nathanwpylestrangeplanet.svg", user:"nathanwpylestrangeplanet"},
+  {img:"assets/img/wawawicomics.svg", user:"wawawicomics"},
+  {img:"assets/img/respondeai.svg", user:"respondeai"},
+  {img:"assets/img/filomoderna.svg", user:"filomoderna"},
+  {img:"assets/img/memeriagourmet.svg", user:"memeriagourmet"},
+];
+
+function Stories() {
+  return (
+    <div class="stories">
+      {storiesArray.map((story) => (
+        <div class="story">
+          <div class="imagem">
+            <img src={story.img} />
+          </div>
+      <div class="usuario">{story.user}</div>
+    </div>
+      ))}
+    
+
+      <div class="setinha">
+        <ion-icon name="chevron-forward-circle"></ion-icon>
+      </div>
+    </div>
+  );
+}
+
+export default Stories;
